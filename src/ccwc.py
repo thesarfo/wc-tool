1550037bb52327061f90c13af81a7f403458010f
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-05 15:33:24 +0000

    now supports the default option i.e no options

--- src/ccwc.py
+++ src/ccwc.py
@@ -5,50 +5,67 @@ def print_bytes(filename):
     try:
         with open(filename, 'rb') as file:
             bytes = len(file.read())
-            print(f"{bytes} {filename}")
+            return bytes
     except FileNotFoundError:
         print(f"Error: File '{filename}' cannot be found.")
+        return 0
 
 def print_lines(filename):
     try:
         with open(filename, 'r') as file:
             lines = sum(1 for line in file)
-            print(f"{lines} {filename}")
+            return lines
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
+        return 0
 
 def print_words(filename):
     try:
         with open(filename, 'r') as file:
             words = sum(len(line.split()) for line in file)
-            print(f"{words} {filename}")
+            return words
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
+        return 0
 
 def print_characters(filename):
     try:
         with open(filename, 'r', encoding='utf-8') as file:
-            chars = len(file.read())
-            print(f"{chars} {filename}")
+            characters = len(file.read())
+            return chars
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
+        return 0
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3 or sys.argv[1] not in ["-c", "-l", "-w", "-m"]:
-        print("Correct usage is: ccwc -c|-l|-w|-m <filename>")
+    if len(sys.argv) < 2:
+        print("Correct usage is: ccwc  -c|-l|-w|-m <filename>")
         sys.exit(1)
-    
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        bytes = print_bytes(filename)
+        lines = print_lines(filename)
+        words = print_words(filename)
+        print(f"{lines: >6} {words: >7} {bytes: >7} {filename}")
+        sys.exit(0)
+
     alt = sys.argv[1]
     filename = sys.argv[2]
 
     if alt == "-c":
-        print_bytes(filename)
+        print(print_bytes(filename), filename)
     elif alt == "-l":
-        print_lines(filename)
+        print(print_lines(filename), filename)
     elif alt == "-w":
-        print_words(filename)
+        print(print_words(filename), filename)
     elif alt == "-m":
         if locale.getpreferredencoding() == 'UTF-8':
-            print_characters(filename)
+            print(print_characters(filename), filename)
         else:
-            print_bytes(filename)
+            print(print_bytes(filename), filename)
+    else:
+        bytes = print_bytes(filename)
+        lines = print_lines(filename)
+        words = print_words(filename)
+        print(f"{lines: >6} {words: >7} {bytes: >7} {filename}")
