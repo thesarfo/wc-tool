0c196de615e5185aa1f158051c4eb15be016ed9f
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-05 14:50:28 +0000

    can now output the number of lines in a file

--- src/ccwc.py
+++ src/ccwc.py
@@ -8,10 +8,23 @@ def print_bytes(filename):
     except FileNotFoundError:
         print(f"Error: File '{filename}' cannot be found.")
 
+def print_lines(filename):
+    try:
+        with open(filename, 'r') as file:
+            line_count = sum(1 for line in file)
+            print(f"{line_count} {filename}")
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+
 if __name__ == "__main__":
-    if len(sys.argv) != 3 or sys.argv[1] != "-c":
-        print("Correct usage is: ccwc -c <filename>")
+    if len(sys.argv) != 3 or sys.argv[1] not in ["-c", "-l"]:
+        print("Correct usage is: ccwc -c|-l <filename>")
         sys.exit(1)
     
+    alt = sys.argv[1]
     filename = sys.argv[2]
-    print_bytes(filename)
+
+    if alt == "-c":
+        print_bytes(filename)
+    elif alt == "-l":
+        print_lines(filename)
